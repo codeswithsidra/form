a561793ed10786495390b54e55386c5dc050566c
Codes with Sidra <sidrabibi6143@gmail.com>
2026-05-21 21:49:40 +0500

    Add files via upload

---
+++ error.php
@@ -0,0 +1,43 @@
+<div style='
+    width: 400px;
+    margin: 100px auto;
+    padding: 20px;
+    background: #ffe5e5;
+    border: 2px solid red;
+    border-radius: 10px;
+    text-align: center;
+    font-family: Arial;
+    box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+'>
+
+    <h2 style='color:red; margin-bottom:10px;'>
+        Duplicate Entry Error
+    </h2>
+
+    <p style='
+        color:#333;
+        font-size:18px;
+        line-height:1.5;
+    '>
+        Duplicate entry for email:
+        <b>$email</b>
+        <br><br>
+        Please use a different email address.
+    </p>
+
+    <a href='index.php'
+       style='
+       display:inline-block;
+       margin-top:15px;
+       padding:10px 20px;
+       background:red;
+       color:white;
+       text-decoration:none;
+       border-radius:5px;
+       '>
+
+       Go Back
+
+    </a>
+
+</div>
